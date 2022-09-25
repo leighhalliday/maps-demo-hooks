4ffc6c81cfe183c00f1983d5976b8ed9b6013184
Leigh Halliday <leighhalliday@gmail.com>
2022-09-25 13:16:05 -0400

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import { useState, useEffect, useRef } from "react";
+import {
+  GoogleMapProvider,
+  useGoogleMap,
+} from "@ubilabs/google-maps-react-hooks";
+
+export default function Index() {
+  return <div>Map</div>;
+}
